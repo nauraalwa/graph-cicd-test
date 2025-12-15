a98bc8d6eb91ca493fafce78e85c6cb25f3899d6
nauraalwa <naura21salwa@gmail.com>
2025-12-15 19:23:16 +0900

    updated code

--- App.jsx
+++ App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vitessssss.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="LE Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">

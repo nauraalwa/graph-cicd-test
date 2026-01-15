af7c8e27c99d4d42e90070139db07f31b336a57c
nauraalwa <naura21salwa@gmail.com>
2026-01-16 08:49:12 +0900

    updated

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/JAN6_GASSHUKU.svg'
-import viteLogo from '/vite.svg'
+import Potatos from '/POTATOESPLS.svg'
 import './App.css'
 import potatoLogo from '/potatoes.png'
 
